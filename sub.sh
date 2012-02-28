a1e3c4899ff81b3eb2f8a40fce066760d03051a8
Dean Pucsek <dean@lightbulbone.com>
2012-02-27 21:27:45 -0800

    Adding qsub script

---
+++ sub.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#PBS -S /bin/bash
+#PBS -N cg
+
+#PBS -l procs=399
+#PBS -l walltime=01:00:00
+#PBS -l mem=4000mb
+#PBS -l pmem=4000mb
+
+mpiexec /home/dpucsek/cg/cg /home/dpucsek/cg/input/Ab.txt 30
